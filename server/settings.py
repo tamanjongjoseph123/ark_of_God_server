0ecc52bff78eb4ee7fbb40028c68f63e7b569c73
tamanjongjoseph123 <tamanjongjoseph8@gmail.com>
2025-11-08 06:34:03 +0100

    latest

--- server/settings.py
+++ server/settings.py
@@ -80,12 +80,12 @@ DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # Password Validation
 AUTH_PASSWORD_VALIDATORS = [
