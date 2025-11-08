04df28487f01974d26b6645e17bccff2b23f9812
tamanjongjoseph123 <tamanjongjoseph8@gmail.com>
2025-11-08 07:28:36 +0100

    latest

--- server/settings.py
+++ server/settings.py
@@ -140,7 +140,7 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5173/",
     'https://b99e3c29.ark-of-god-admin-new.pages.dev/'
 ]
 
